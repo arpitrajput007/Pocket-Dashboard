ab17bb4479ee0e2fbe5d5dd888ea84641b43f0e8
Arpit Kumar <79630115+arpitrajput007@users.noreply.github.com>
2026-06-04 01:04:34 +0530

    Revert "feat(leads): add LeadInbox with category filtering and sub-categories"
    This reverts commit 1d7ef3b0c6a35bf5155e273da953bab2b284060d.

--- src/App.jsx
+++ src/App.jsx
@@ -41,8 +41,6 @@ const PersonalPanel = lazy(() => import('./components/PersonalPanel'));
 const PrivacyPolicy = lazy(() => import('./components/PrivacyPolicy'));
 const TermsAndConditions = lazy(() => import('./components/TermsAndConditions'));
 const ContactPage = lazy(() => import('./components/ContactPage'));
-const AerediumTargets = lazy(() => import('./components/AerediumTargets'));
-const LeadInbox = lazy(() => import('./components/LeadInbox'));
 
 const LoadingFallback = () => (
   <div style={{
@@ -204,8 +202,6 @@ export default function App() {
           <Route path="/privacy" element={<PrivacyPolicy />} />
           <Route path="/terms" element={<TermsAndConditions />} />
           <Route path="/contact-us" element={<ContactPage />} />
-          <Route path="/aeredium-targets" element={<AerediumTargets />} />
-          <Route path="/lead-inbox" element={<LeadInbox />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Suspense>
